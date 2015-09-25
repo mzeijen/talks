f697a651cfbffd3726fe76f80ddd1de2c83b5a4a
Maurice Zeijen <mzeijen@bol.com>
2015-09-25 11:05:51 +0200

    Sanitized the helpful code from Patrick so that it now doesn't refer to him any more as if it where his presentations. He is mentioned though as the creator of this presentation 'framework'. Also removed his presentations.

--- subprojects/2015-05-20_Groovy-Gradle_bol.com/examples/gradle-groovy-spock/src/main/groovy/com/bol/example/FizzBuzz.groovy
+++
@@ -1,7 +0,0 @@
-package com.bol.example
-
-class FizzBuzz {
-    List<String> convert(List<Integer> input) {
-        input.collect { (it % 3 == 0 ? 'Fizz' : '') + (it % 5 == 0 ? 'Buzz' : '') }
-    }
-}
